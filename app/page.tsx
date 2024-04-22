2d5e13bcd20960990fac15238628173a0f87b7f5
Diogo Doreto <diogo@doreto.com.br>
2024-04-22 22:32:22 +0200

    add links to pdf and source code

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as R from "ramda";
 import { AllRotations, AllStances, Rotation, Stance } from "./lib/constants";
 import { RadioButton } from "./components/ui";
 import { SvgCourt, SvgPlayer } from "./components/svg";
 import { plays } from "./lib/plays";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faCode,
+  faPrint,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
+  const dialog = useRef<HTMLDialogElement>(null);
   const [rotation, setRotation] = useState(Rotation.R1);
   const [stance, setStance] = useState(Stance.Neutral);
   const itemsOnCanvas = R.groupBy(R.prop("kind"), plays[rotation][stance]);
@@ -25,11 +33,53 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="bg-marine flex min-h-screen flex-col items-center justify-between">
-      <svg
-        viewBox="0 0 1250 1150"
-        style={{ maxHeight: "calc(100dvh - 150px)" }}
+    <main className="bg-marine min-h-screen flex flex-col items-center">
+      <div className="w-full bg-royal flex items-center h-10">
+        <h1
+          className="grow px-4 font-medium text-lg"
+          style={{ fontVariantCaps: "small-caps" }}
+        >
+          Volleyball Theory
+        </h1>
+        <button
+          title="Open menu"
+          className="hover:bg-navy w-10 h-10"
+          onClick={() => dialog.current?.showModal()}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </div>
+
+      <dialog
+        ref={dialog}
+        className="min-h-screen mr-0 bg-navy text-white flex flex-col p-4 gap-4"
       >
+        <button
+          className="hover:bg-royal p-2 rounded-lg"
+          onClick={() => dialog.current?.close()}
+        >
+          <FontAwesomeIcon icon={faXmark} className="mr-2" />
+          Close
+        </button>
+        <a
+          target="_blank"
+          href="https://drive.google.com/file/d/1EhrO0MxBVK0NmYTvYrY7V2Bt9pxGOC7Z/view?usp=drive_link"
+          className="border-white border-2 rounded-lg p-4 hover:bg-royal"
+        >
+          <FontAwesomeIcon icon={faPrint} className="mr-4" />
+          Print version (PDF)
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/DiogoDoreto/volleyball-theory"
+          className="border-white border-2 rounded-lg p-4 hover:bg-royal"
+        >
+          <FontAwesomeIcon icon={faCode} className="mr-4" />
+          Source Code
+        </a>
+      </dialog>
+
+      <svg className="grow w-full" viewBox="0 0 1250 1150">
         <g transform="translate(175,-700)">
           <SvgCourt />
           {players.map(({ x, y, position }) => (
@@ -38,7 +88,7 @@ export default function Home() {
         </g>
       </svg>
 
-      <div className="w-full flex flex-col gap-4 p-6">
+      <div className="w-full box-border flex flex-col gap-4 p-6">
         <div className="flex gap-2 items-stretch h-10">
           {AllRotations.map((value) => (
             <RadioButton
